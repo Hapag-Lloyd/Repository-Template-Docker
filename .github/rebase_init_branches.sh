c903a086493f7bf323541425a2acad8750883e85
Matthias Kay <matthias.kay@hlag.com>
2024-05-13 21:06:36 +0200

    INIT

---
+++ .github/rebase_init_branches.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#
+# This script rebases all init branches on the main branch.
+#
+
+git checkout main
+git pull
+
+# for all init branches
+for branch in $(git branch --list "init-*"); do
+  git checkout "$branch"
+  git rebase main
+done
